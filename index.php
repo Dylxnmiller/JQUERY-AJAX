8b0863b5780125b937b2b2c594b070927ab922e5
Dylan Miller <91589107+Dylxnmiller@users.noreply.github.com>
2022-02-08 19:15:35 +0000

    Test line added to php

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 
       <p>Below are links to jQuery AJAX examples</p>
 
-      <p><a href="ja_load.html" target="_blank">ja_load.html</a></p>
+      <p>This is a test</p>
+
+      <p><a href="ja_load.html"target="_blank">ja_load.html</a</p>
       <p><a href="ja_get.html" target="_blank">ja_get.html</a></p>
       <p><a href="ja_post.html" target="_blank">ja_post.html</a></p>
   </body>
